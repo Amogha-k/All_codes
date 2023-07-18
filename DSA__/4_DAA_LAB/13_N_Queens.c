7e149e4e9c02c1acd16f4dfbfe6b1ec40347f993
John Doe <johndoe@email.com>
2023-07-18 22:33:30 +0530

    modified

--- DSA__/4_DAA_LAB/13_N_Queens.c
+++ DSA__/4_DAA_LAB/13_N_Queens.c
@@ -1,51 +1,55 @@
-#include <stdio.h>
-#include <math.h>
-#include <stdlib.h>
-#define false 0
-#define true 1
+
+//PROGRAM:
+
+//backtracking
+#include<stdio.h>
+#include<math.h>
+#define FALSE 0
+#define TRUE 1
 
 int x[20];
-
-int place(int k, int i)
+int place(int k,int i)
 {
-    int j;
-    for (j = 1; j <= k; j++)
-    {
-        if ((x[i] == j) || ((abs(x[j] - i)) == abs(j - k)))
-            return false;
-    }
-    return true;
+	int j;
+	for(j=1;j<=k;j++)
+	{
+		if((x[j]==i)||(abs(x[j]-i)==abs(j-k)))
+		return FALSE;
+	}
+return TRUE;
 }
 
-void nqueens(int k, int n)
+void nqueens(int k,int n)
 {
-    int i, a;
-    for (i = 1; i <= n; i++)
-    {
-        if (place(k, i))
-        {
-            x[k] = i;
-            if (k == n)
-            {
-                for (a = 1; a <= n; a++)
-                {
-                    printf("%d", x[a]);
-                }
-                printf("\n");
-            }
-        }
-        else
-        {
-            nqueens(k + 1, n);
-        }
-    }
+	int i,a;
+	for(i=1;i<=n;i++)
+	{
+		if(place(k,i))
+		{
+			x[k]=i;
+			if(k==n)
+			{
+				for(a=1;a<=n;a++)
+					printf("%d\t",x[a]);
+				printf("\n");
+			}
+			else
+			nqueens(k+1,n);
+		}
+	}
 }
-
 void main()
 {
-    int n;
-    printf("Enter the numbers of queens");
-    scanf("%d", &n);
-    printf("\nThe solution to nqueens problem is : \n");
-    nqueens(1, n);
+	int n;
+	printf("\nEnter the number of queens:");
+	scanf("%d",&n);
+	printf("\n The solution to N Queens problem is: \n");
+	nqueens(1,n);
 }
+
+/* Sample output*/
+
+// Enter the number of queens:4
+
+//  The solution to N Queens problem is:
+// 3       1       4       2
